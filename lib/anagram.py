e3b02e9e24141e91d5003ae4fcee73115e85cc38
jeremykariuki <mirujeremy@gmail.com>
2024-05-27 12:52:11 +0300

    last commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+
+    def match(self, words):
+        return [anagram for anagram in words if sorted(anagram.lower()) == self.sorted_word]
